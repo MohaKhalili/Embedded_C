8a49084574d07618b1da49c3494d210f0f0b4379
mohakhalili <md.kh71@gmail.com>
2020-03-19 20:57:51 +0330

    first program for host

---
+++ host/000TestProject/main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 19, 2020
+ *      Author: Moha
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World\n");
+	printf("Press 'Enter' to exit this application\n");
+	getchar();
+	return 0;
+}
