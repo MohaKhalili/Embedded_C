bd2e7dd013d3acc7e85d6e861269ff76f1e36530
mohakhalili <md.kh71@gmail.com>
2020-03-24 18:13:54 +0430

    TestProject for host machine added

---
+++ host/000TestProject/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 24, 2020
+ *      Author: Moha
+ */
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello world\n");
+
+	printf("Press 'Enter' to exit this application\n");
+
+	getchar();
+
+	return 0;
+}
